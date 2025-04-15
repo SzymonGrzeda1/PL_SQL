a7cedca38b8cf145456d1e0d2fecb9d6cfa979ca
SzymonGrzeda1 <szymon.marek.grzeda@gmail.com>
2025-04-15 07:43:05 +0200

    Update trace.sql

--- SQL/trace.sql
+++ SQL/trace.sql
@@ -1,2 +1,7 @@
 grant SELECT_CATALOG_ROLE to hr;
 grant SELECT ANY DICTIONARY to hr;
+
+explain plan for
+select count(*) from f_fact where d1 = 2 and (d2 = 3 or d3 = 4) and d4 IN (10,11,12);
+
+select * from table(dbms_xplan.display);
